9c6b7b20a7aa555baf8d3a8dc20b5f25982dbe30
spgihuij <servihuijbregts@gmail.com>
2019-03-21 16:11:53 +0100

    Startscherm pagina: Begin aan code van activity1

--- app/src/main/java/com/example/navigationdrawer/SociaalNetwerkFragment.java
+++ app/src/main/java/com/example/navigationdrawer/SociaalNetwerkFragment.java
@@ -1,5 +1,6 @@
 package com.example.navigationdrawer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,21 +8,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageButton;
 
 public class SociaalNetwerkFragment extends Fragment {
     @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_sociaalnetwerk,container,false);
-      /**  TextView tekst1TextView = (TextView) findViewById(R.id.tekst1TextView);
-        TextView tekst2TextView = (TextView) findViewById(R.id.tekst2TextView);
-        TextView tekst3TextView = (TextView) findViewById(R.id.tekst3TextView);
-        TextView tekst4TextView = (TextView) findViewById(R.id.tekst4TextView);
-        TextView tekst5TextView = (TextView) findViewById(R.id.tekst5TextView);
-        TextView tekst6TextView = (TextView) findViewById(R.id.tekst6TextView);
-**/
-      //afb1ImageButton = (ImageButton) findViewById(R.id.afb1ImageButton);
 
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sociaalnetwerk, container, false);
     }
 }
