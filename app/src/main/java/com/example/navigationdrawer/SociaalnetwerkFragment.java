e4054d00a945fbac9bd1e1229f394cca4437b0ca
spgihuij <servihuijbregts@gmail.com>
2019-03-21 14:22:43 +0100

    Startscherm pagina: Textviews gemaakt

--- app/src/main/java/com/example/navigationdrawer/SociaalnetwerkFragment.java
+++ app/src/main/java/com/example/navigationdrawer/SociaalnetwerkFragment.java
@@ -7,11 +7,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SociaalnetwerkFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_sociaalnetwerk,container,false);
+        TextView tekst1TextView = (TextView) findViewById(R.id.tekst1TextView);
+        TextView tekst2TextView = (TextView) findViewById(R.id.tekst2TextView);
+        TextView tekst3TextView = (TextView) findViewById(R.id.tekst3TextView);
+        TextView tekst4TextView = (TextView) findViewById(R.id.tekst4TextView);
+        TextView tekst5TextView = (TextView) findViewById(R.id.tekst5TextView);
+        TextView tekst6TextView = (TextView) findViewById(R.id.tekst6TextView);
+
     }
 }
