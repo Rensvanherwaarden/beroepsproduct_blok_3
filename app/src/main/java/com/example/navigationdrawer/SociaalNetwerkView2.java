c7cd9fa2ef1587f7062c84e13ce4bb3814ae5eb5
spgihuij <servihuijbregts@gmail.com>
2019-03-29 15:40:58 +0100

    Fragment 3 idee als comment toegevoegd en layout gemaakt in het XML bestand

---
+++ app/src/main/java/com/example/navigationdrawer/SociaalNetwerkView2.java
@@ -0,0 +1,30 @@
+package com.example.navigationdrawer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SociaalNetwerkView2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sociaal_netwerk_view2);
+        /**
+         *
+         *
+         * Verhaal creeeren door items uit spinners.
+         * We zullen eens gaan kijken welk sociaal netwerk het beste bij jou past.
+         * Als jij binnenkomt op een feest ben je als eerste te vinden bij: ..... .
+         * Als jij de loterij wint koop jij: .... .
+         * Jij wilde .... worden als je oud was.
+         * Jou favoriete huisdier is een ..... .
+         * De sport .... vind jij het leukst.
+         * Op TV kijk jij graag naar: ..... .
+         * En op het internet zit je het liefst op: ..... .
+         * Als laatste vind jij het bordspel: .... echt het leukst!
+         * Op basis van deze gegevens lijkt dit netwerk ons het leukste netwerk voor jou: ......!
+         *
+         * Genereren welk sociaal netwerk hierbij past.
+         */
+    }
+}
