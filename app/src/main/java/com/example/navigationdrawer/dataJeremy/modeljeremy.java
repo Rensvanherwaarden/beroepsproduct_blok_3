0b2cabf1b2c9a76a548204f6e8822d7e635ab1cb
Jeremy <jeremy.bovenkamp@gmail.com>
2019-04-05 16:04:56 +0200

    database toegevoegd 5-4-2019 16:04

--- app/src/main/java/com/example/navigationdrawer/dataJeremy/modeljeremy.java
+++ app/src/main/java/com/example/navigationdrawer/dataJeremy/modeljeremy.java
@@ -2,9 +2,31 @@ package com.example.navigationdrawer.dataJeremy;
 
 public class modeljeremy {
 
-    public String naam;
+    public String clientid, naam, geboortedatum, woonplaats, interesse, hobby, expertise, wachtwoord;
+
+    public String getClientid() {return clientid;}
+    public void setClientid(String id) {this.clientid = clientid;}
 
     public String getNaam() {return naam;}
-
     public void setNaam(String naam) {this.naam = naam;}
+
+    public String getGeboortedatum() {return geboortedatum;}
+    public void setGeboortedatum(String geboortedatum) {this.geboortedatum = geboortedatum;}
+
+    public String getWoonplaats() {return woonplaats;}
+    public void setWoonplaats(String woonplaats) {this.woonplaats = woonplaats;}
+
+    public String getInteresse() {return interesse;}
+    public void setInteresse(String interesse) {this.interesse = interesse;}
+
+    public String getHobby() {return hobby;}
+    public void setHobby(String hobby) {this.hobby = hobby;}
+
+    public String getExpertise() {return expertise;}
+    public void setExpertise(String expertise) {this.expertise = expertise;}
+
+    public String getWachtwoord() {return wachtwoord;}
+    public void setWachtwoord(String wachtwoord) {this.wachtwoord = wachtwoord;}
+
+
 }
